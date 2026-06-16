60c0b4a2687e4029bef874bad56a2ead3afbc286
beeynow <beeynow@gmail.com>
2026-06-16 17:43:35 +0100

    db + auth

--- pages/api/auth/session/login.js
+++ pages/api/auth/session/login.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
       throw new Error('Firebase Admin is not initialized');
     }
 
-    const decodedToken = await admin.auth().verifyIdToken(idToken, true);
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
     const session = await createServerSession({
       req,
       res,
